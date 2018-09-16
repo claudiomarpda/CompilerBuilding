bcbceea710a25ef3630f709b3fe7b88c9ee3be73
Claudiomar Araújo <claudiomar.araujo@lavid.ufpb.br>
2018-09-16 10:23:13 -0300

    Pascal file for test

---
+++ input/input9.pas
@@ -0,0 +1,14 @@
+program example;
+
+var
+a, b: real;
+c : boolean;
+d : integer;
+
+begin
+    a := 2 + 2;
+    b := a + 2;
+    b := a + a;
+    c := b > a;
+    d := 10 + c;
+end.
---
+++ output/tokens-input9.pas
@@ -0,0 +1,53 @@
+                     NAME                      TYPE                      LINE
+
+                  program                   Keyword                         1
+                  example                Identifier                         1
+                        ;                 Delimiter                         1
+                      var                   Keyword                         3
+                        a                Identifier                         4
+                        ,                 Delimiter                         4
+                        b                Identifier                         4
+                        :                 Delimiter                         4
+                     real                   Keyword                         4
+                        ;                 Delimiter                         4
+                        c                Identifier                         5
+                        :                 Delimiter                         5
+                  boolean                   Keyword                         5
+                        ;                 Delimiter                         5
+                        d                Identifier                         6
+                        :                 Delimiter                         6
+                  integer                   Keyword                         6
+                        ;                 Delimiter                         6
+                    begin                   Keyword                         8
+                        a                Identifier                         9
+                       :=               Attribution                         9
+                        2                   Integer                         9
+                        +         Additive Operator                         9
+                        2                   Integer                         9
+                        ;                 Delimiter                         9
+                        b                Identifier                        10
+                       :=               Attribution                        10
+                        a                Identifier                        10
+                        +         Additive Operator                        10
+                        2                   Integer                        10
+                        ;                 Delimiter                        10
+                        b                Identifier                        11
+                       :=               Attribution                        11
+                        a                Identifier                        11
+                        +         Additive Operator                        11
+                        a                Identifier                        11
+                        ;                 Delimiter                        11
+                        c                Identifier                        12
+                       :=               Attribution                        12
+                        b                Identifier                        12
+                        >       Relational Operator                        12
+                        a                Identifier                        12
+                        ;                 Delimiter                        12
+                        d                Identifier                        13
+                       :=               Attribution                        13
+                       10                   Integer                        13
+                        +         Additive Operator                        13
+                        c                Identifier                        13
+                        ;                 Delimiter                        13
+                      end                   Keyword                        14
+                        .                 Delimiter                        14
